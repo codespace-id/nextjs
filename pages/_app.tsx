464e483938f076e7b95f36c7dc6935807c250a41
Muhammad Firdaus <mbex71@gmail.com>
2022-09-02 19:36:41 +0700

    add ant design css framework

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import 'antd/dist/antd.css'
 
 //Create Query Client
 const queryClient = new QueryClient()
