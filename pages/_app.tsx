6249da9b52fa18956fbccf6795e4f16f5fb16dbe
Muhammad Firdaus <mbex71@gmail.com>
2022-09-02 23:42:36 +0700

    add jest config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,19 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import 'antd/dist/antd.css'
+import configs from '../configs'
 
 //Create Query Client
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const { env } = configs
     return (
         // Provide the client to your App
         <QueryClientProvider client={queryClient}>
             <Component {...pageProps} />
-            <ReactQueryDevtools />
+            {env == 'development' ? <ReactQueryDevtools /> : null}
         </QueryClientProvider>
     )
 }
